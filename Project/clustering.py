e44fd356eea845b65e2eaf635b44873182a853a7
Giuliana Prinzi <giulianaprinzi@MacBook-Pro-di-Giuliana.local>
2023-12-11 17:52:25 +0100

    boh

--- Project/clustering.py
+++ Project/clustering.py
@@ -1,14 +1,17 @@
 import pandas as pd
 from sklearn.utils import resample
+from sklearn.cluster import KMeans
 
-train = pd.read_csv('data/test.csv')
-
+train = pd.read_csv('data/train.csv')
 X_train = train.drop('Tag', axis=1)  # X_train contains only the features
+columns_to_drop = ['Track', 'Artist'] #prendiamo solo i numerici
+X_train_numerical = X_train.drop(columns=columns_to_drop)
 y_train = train['Tag'] # y_train contains only the targets
 
 # rebalance the element in the test set such that for each tag there will be the same amount of entries
 test = pd.read_csv('data/test.csv')
 X_test = test.drop('Tag', axis=1)
+X_test_numerical = X_test.drop(columns=columns_to_drop)
 y_test = test['Tag']  
 
 tag_count = y_test.value_counts()
@@ -23,9 +26,12 @@ for tag in tag_count.index:
 X_test = test_resampled.drop('Tag', axis=1)
 y_test = test_resampled['Tag']
 
-################# TODO HERE ##############
-
-
+################# TO DO HERE ##############
+number_of_clusters=5
+kmeans = KMeans(n_clusters=number_of_clusters)
+kmeans.fit(X_train)
+test_clusters = kmeans.predict(X_test_numerical)
+print(test_clusters[:10])
 
 
 ##########################################
