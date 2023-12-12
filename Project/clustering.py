b52b992694285077a253e1291623c9951b12f1e0
lauraaconcari <concari.1890490@studenti.uniroma1.it>
2023-12-12 10:45:24 +0100

    lo ha corretto chat gpt non so che ho fatto

--- Project/clustering.py
+++ Project/clustering.py
@@ -1,21 +1,26 @@
 import pandas as pd
 from sklearn.utils import resample
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
 
-train = pd.read_csv('data/train.csv')
-X_train = train.drop('Tag', axis=1)  # X_train contains only the features
-columns_to_drop = ['Track', 'Artist'] #prendiamo solo i numerici
+# Read training data
+train = pd.read_csv('Project/data/train.csv')
+X_train = train.drop('Tag', axis=1)
+columns_to_drop = ['Track', 'Artist']
 X_train_numerical = X_train.drop(columns=columns_to_drop)
-y_train = train['Tag'] # y_train contains only the targets
 
-# rebalance the element in the test set such that for each tag there will be the same amount of entries
-test = pd.read_csv('data/test.csv')
+# Read and preprocess test data
+test = pd.read_csv('Project/data/test.csv')
 X_test = test.drop('Tag', axis=1)
 X_test_numerical = X_test.drop(columns=columns_to_drop)
-y_test = test['Tag']  
 
-tag_count = y_test.value_counts()
+# One-hot encoding for categorical variables
+X_train_encoded = pd.get_dummies(X_train_numerical)
+X_test_encoded = pd.get_dummies(X_test_numerical)
+
+# Rebalance the test set
 test_resampled = pd.DataFrame(columns=test.columns)
+tag_count = test['Tag'].value_counts()
 min_count = tag_count.min()
 
 for tag in tag_count.index:
@@ -23,15 +28,21 @@ for tag in tag_count.index:
     tag_resampled = resample(tags, replace=True, n_samples=min_count, random_state=42)
     test_resampled = pd.concat([test_resampled, tag_resampled])
 
-X_test = test_resampled.drop('Tag', axis=1)
-y_test = test_resampled['Tag']
+X_test_resampled = test_resampled.drop('Tag', axis=1)
 
-################# TO DO HERE ##############
-number_of_clusters=5
-kmeans = KMeans(n_clusters=number_of_clusters)
-kmeans.fit(X_train)
-test_clusters = kmeans.predict(X_test_numerical)
+# Standardize features
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train_encoded)
+X_test_scaled = scaler.transform(X_test_resampled)
+
+# Apply k-means clustering
+number_of_clusters = 5
+kmeans = KMeans(n_clusters=number_of_clusters, random_state=42)
+
+# Fit on training data
+kmeans.fit(X_train_scaled)
+
+# Predict clusters on test data
+test_clusters = kmeans.predict(X_test_scaled)
 print(test_clusters[:10])
 
-
-##########################################
